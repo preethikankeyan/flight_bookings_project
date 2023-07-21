b73b2d13d92e255c4642ce6dd3e8f5b0989a0e8b
preethikankeyan <139895490+preethikankeyan@users.noreply.github.com>
2023-07-20 22:17:44 -0400

    Update admin.py to unregister a few buttons and include few views

--- flight_bookings_app/admin.py
+++ flight_bookings_app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User, Group
 from .models import Flight, Booking
+
+admin.site.site_header = 'Flight booking site administration'
+admin.site.index_title = 'Click on the Bookings button to view, edit and change all Bookings, Click on the Flights button to view, edit and change all Flights, Click on the Users button to view, edit and change all Users'
 
 class FlightAdmin(admin.ModelAdmin):
     list_display = ('flightnumber', 'departure_airport', 'arrival_airport', 'departure_date', 'departure_time', 'available_seats')
@@ -8,6 +13,16 @@ class FlightAdmin(admin.ModelAdmin):
 
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('booking_id', 'flight_number', 'user', 'passenger_name', 'passenger_email', 'num_passengers')
+    list_filter = ('flight_number', 'user')
+    search_fields = ('booking_id', 'user__username', 'passenger_name', 'passenger_email')
 
+class MyUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff',)
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, MyUserAdmin)
 admin.site.register(Flight, FlightAdmin)
 admin.site.register(Booking, BookingAdmin)
